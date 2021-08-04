ec74556bed7bc8701eed603877662cbff2fdd665
osmannkartall <okartal3434@gmail.com>
2021-08-04 10:52:12 +0300

    add colors

---
+++ web/src/constants/colors.js
@@ -0,0 +1,8 @@
+export const PRIMARY = '#1563ff';
+export const SECONDARY = '#d0e0ff';
+export const BLACK = '#000000';
+export const WHITE = '#ffffff';
+export const GRAY1 = '#333333'; // font
+export const GRAY2 = '#7f7f86'; // modals, icons, inactive states
+export const GRAY3 = '#707070'; // border
+export const GRAY4 = '#f5f5f5'; // low contrast background: inner divs
