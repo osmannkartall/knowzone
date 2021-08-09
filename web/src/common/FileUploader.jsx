c57e6a780d7174966465458080055aeaa8d25b1e
osmannkartall <okartal3434@gmail.com>
2021-08-09 16:22:50 +0300

    center image on container

--- web/src/common/FileUploader.jsx
+++ web/src/common/FileUploader.jsx
@@ -14,6 +14,7 @@ const useStyles = makeStyles((theme) => ({
     height: 300,
     padding: 4,
     boxSizing: 'border-box',
+    justifyContent: 'center',
   },
   btn: {
     position: 'absolute',
