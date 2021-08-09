a4c393b8db19c886c8588b4237cae70d992ab871
osmannkartall <okartal3434@gmail.com>
2021-08-09 10:26:54 +0300

    pass placeholder value as prop

--- web/src/common/TagPicker/TagPicker.jsx
+++ web/src/common/TagPicker/TagPicker.jsx
@@ -3,14 +3,24 @@ import ReactTagInput from '@pathofdev/react-tag-input';
 import '@pathofdev/react-tag-input/build/index.css';
 import './TagPicker.css';
 
-const TagPicker = ({ tags, setTags, readOnly }) => (
-  <ReactTagInput
-    tags={tags}
-    onChange={(newTags) => setTags(newTags)}
-    removeOnBackspace
-    readOnly={readOnly}
-    placeholder="Type a topic and press enter to add"
-  />
-);
+const TagPicker = ({ tags, setTags, readOnly, placeholder, required }) => {
+  let placeholderText = placeholder;
+  if (placeholderText && required)
+    placeholderText += ' *';
+
+  return (
+    <ReactTagInput
+      tags={tags}
+      onChange={(newTags) => setTags(newTags)}
+      removeOnBackspace
+      readOnly={readOnly}
+      placeholder={placeholderText}
+    />
+  );
+};
+
+TagPicker.defaultProps = {
+  placeholder: 'Type a topic and press enter to add',
+};
 
 export default TagPicker;
