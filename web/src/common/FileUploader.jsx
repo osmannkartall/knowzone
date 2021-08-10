c02b26f92a164bc3269fe716a5e8d7a9057087db
osmannkartall <okartal3434@gmail.com>
2021-08-10 12:35:22 +0300

    fix positioning of preview element

--- web/src/common/FileUploader.jsx
+++ web/src/common/FileUploader.jsx
@@ -4,29 +4,31 @@ import { useDropzone } from 'react-dropzone';
 import CloudUpload from '@material-ui/icons/CloudUpload';
 import { GRAY2, GRAY3, GRAY4, PRIMARY } from '../constants/colors';
 
+const NUM_MAX_FILES = 2;
+const THUMB_WIDTH = 300;
+const THUMB_HEIGHT = 300;
+const activeStyle = { borderColor: '#2196f3' };
+const acceptStyle = { borderColor: PRIMARY };
+const rejectStyle = { borderColor: '#ff1744' };
+
 const useStyles = makeStyles((theme) => ({
   thumb: {
     display: 'inline-flex',
     borderRadius: 2,
     border: `1px solid ${GRAY3}`,
-    marginRight: theme.spacing(4),
-    width: `calc(50% - ${theme.spacing(4)}px)`,
-    height: 300,
+    width: THUMB_WIDTH,
+    height: THUMB_HEIGHT,
     padding: 4,
     boxSizing: 'border-box',
-    justifyContent: 'center',
   },
-  btn: {
-    position: 'absolute',
-    bottom: '0%',
-    left: '50%',
-    transform: 'translate(-50%, 0%)',
-    width: '100%',
+  thumbBtn: {
+    width: THUMB_WIDTH,
+    margin: theme.spacing(1, 0),
     opacity: 0.5,
-    transition: '.5s ease',
     '&:hover': {
       opacity: 1,
     },
+    alignSelf: 'center',
   },
   img: {
     display: 'block',
@@ -37,13 +39,18 @@ const useStyles = makeStyles((theme) => ({
     display: 'flex',
     flexDirection: 'row',
     flexWrap: 'wrap',
-    marginTop: theme.spacing(3),
+    justifyContent: 'flex-start',
+    marginTop: theme.spacing(1),
   },
   thumbInner: {
     display: 'flex',
     minWidth: 0,
     overflow: 'hidden',
-    position: 'relative',
+  },
+  thumbOuter: {
+    display: 'flex',
+    flexDirection: 'column',
+    marginRight: theme.spacing(4),
   },
 }));
 
@@ -62,12 +69,6 @@ const baseStyle = {
   outline: 'none',
   transition: 'border .24s ease-in-out',
 };
-
-const activeStyle = { borderColor: '#2196f3' };
-const acceptStyle = { borderColor: PRIMARY };
-const rejectStyle = { borderColor: '#ff1744' };
-
-const NUM_MAX_FILES = 2;
 
 const FileUploader = ({ files, setFiles }) => {
   const classes = useStyles();
@@ -118,22 +119,24 @@ const FileUploader = ({ files, setFiles }) => {
   };
 
   const thumbs = files.map((file) => (
-    <div className={classes.thumb} key={file.name}>
-      <div className={classes.thumbInner}>
-        <img
-          src={file.preview}
-          className={classes.img}
-          alt="img"
-        />
-        <Button
-          className={classes.btn}
-          variant="contained"
-          color="primary"
-          onClick={() => onClickDelete(file)}
-        >
-          Delete
-        </Button>
+    <div className={classes.thumbOuter} key={file.name}>
+      <div className={classes.thumb}>
+        <div className={classes.thumbInner}>
+          <img
+            src={file.preview}
+            className={classes.img}
+            alt="img"
+          />
+        </div>
       </div>
+      <Button
+        className={classes.thumbBtn}
+        variant="contained"
+        color="primary"
+        onClick={() => onClickDelete(file)}
+      >
+        Delete
+      </Button>
     </div>
   ));
 
