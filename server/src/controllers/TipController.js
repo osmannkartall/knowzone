23aa9ab50f44ecfa42821103a2753aa0db2a45a1
Osman Kartal <okartal3434@gmail.com>
2021-08-19 14:12:02 +0300

    fix the name of field

--- server/src/controllers/TipController.js
+++ server/src/controllers/TipController.js
@@ -7,7 +7,7 @@ const tipRepository = new TipRepository(TipModel);
 const create = (req, res) => {
   // example usage: Don't send all the data in req.body to the service/repository layer.
   const tip = {
-    owner: req.body.ownerId,
+    owner: req.body.owner,
     links: req.body.links,
     topics: req.body.topics,
     description: req.body.description,
