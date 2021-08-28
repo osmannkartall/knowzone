d8bea8954bd33154e64a4c42ac3598d5f019f358
Yusuf AKDAS <yus.akdas@gmail.com>
2021-08-28 18:48:47 +0300

    Create template 404 page.

--- web/src/components/NotFound.jsx
+++ web/src/components/NotFound.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './NotFound.css';
 
 const NotFound = () => (
-  <div>
-    <h1>404 - Not Found!</h1>
-    <Link to="/">
-      Go Home
-    </Link>
+  <div id="notfound">
+    <div className="notfound">
+      <div className="notfound-404">
+        <h1>:(</h1>
+      </div>
+      <h2>404 - Page not found</h2>
+      <p>
+        The page you are looking for might have been removed or is temporarily unavailable.
+      </p>
+      <button type="button">home page</button>
+    </div>
   </div>
 );
 
