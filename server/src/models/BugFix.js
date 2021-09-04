86a554adbd8bc49b95e0517834d535ff5d19a21b
Osman Kartal <okartal3434@gmail.com>
2021-09-05 00:07:03 +0300

    add mongo validation to the model of bugfix post

--- server/src/models/BugFix.js
+++ server/src/models/BugFix.js
@@ -1,18 +1,101 @@
-const mongoose = require('mongoose');
+const { Schema, model } = require('mongoose');
 
-const bugFixSchema = new mongoose.Schema(
+const MAX_NUM_TOPICS = 5;
+const MIN_NUM_TOPICS = 1;
+const MAX_NUM_IMAGES = 2;
+const MAX_NUM_LINKS = 5;
+const MAX_LEN_DESCRIPTION = 1000;
+const MAX_LEN_ERROR = 4000;
+const MAX_LEN_SOLUTION = 4000;
+
+const ACCEPTED_MIMES = ['image/png', 'image/jpeg', 'image/gif'];
+
+function isMimeValid(acceptedMimes, mime) {
+  return acceptedMimes.includes(mime);
+}
+
+function isLengthBetween(items, max, min = 0) {
+  return items && items.length >= min && items.length <= max;
+}
+
+const validateMime = () => [
+  (mime) => isMimeValid(ACCEPTED_MIMES, mime),
+  (props) => `Unsupported mime-type, RECEIVED: ${props.value}`,
+];
+
+const validateArrayLength = (name, max, min = 0) => [
+  (items) => isLengthBetween(items, max, min),
+  (props) => `Number of ${name} should be in [${min}, ${max}], RECEIVED: ${props.value.length}`,
+];
+
+const maxLengthMessage = (len) => `Received value longer than maximum allowed length(${len}).`;
+
+const bugFixSchema = new Schema(
   {
-    owner: { id: mongoose.Schema.Types.ObjectId, username: String, name: String },
-    links: [String],
-    topics: [String],
-    error: String,
-    solution: String,
-    description: String,
-    images: [{
-      name: String,
-      content: Buffer,
-      mime: String,
-    }],
+    owner: {
+      id: {
+        type: Schema.Types.ObjectId,
+        required: true,
+      },
+      username: {
+        type: String,
+        required: true,
+        match: /^@?([a-z0-9_]){1,15}$/,
+      },
+      name: {
+        type: String,
+        required: true,
+        // TODO: match: /^([a-zA-Z]+\s)[a-zA-Z]+$/,
+      },
+    },
+    links: {
+      type: [String],
+      validate: validateArrayLength('links', MAX_NUM_LINKS),
+    },
+    topics: {
+      type: [
+        {
+          type: String,
+          match: /^@?([a-z0-9-]){1,30}$/,
+          lowercase: true,
+        },
+      ],
+      required: true,
+      validate: validateArrayLength('topics', MAX_NUM_TOPICS, MIN_NUM_TOPICS),
+    },
+    description: {
+      // trim: true,
+      type: String,
+      required: true,
+      maxLength: [MAX_LEN_DESCRIPTION, maxLengthMessage(MAX_LEN_DESCRIPTION)],
+    },
+    error: {
+      type: String,
+      required: true,
+      maxLength: [MAX_LEN_ERROR, maxLengthMessage(MAX_LEN_ERROR)],
+    },
+    solution: {
+      type: String,
+      required: true,
+      maxLength: [MAX_LEN_SOLUTION, maxLengthMessage(MAX_LEN_SOLUTION)],
+    },
+    images: {
+      type: [
+        {
+          name: String,
+          content: {
+            type: Buffer,
+            required: true,
+          },
+          mime: {
+            type: String,
+            required: true,
+            validate: validateMime(),
+          },
+        },
+      ],
+      validate: validateArrayLength('images', MAX_NUM_IMAGES),
+    },
   },
   { timestamps: true },
 );
@@ -25,6 +108,6 @@ bugFixSchema.set('toJSON', {
   },
 });
 
-const BugFix = mongoose.model('BugFix', bugFixSchema);
+const BugFix = model('BugFix', bugFixSchema);
 
 module.exports = BugFix;
