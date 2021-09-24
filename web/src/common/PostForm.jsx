6d959251828a5bdc3d5ae09f8e7abed428e75f89
Osman Kartal <okartal3434@gmail.com>
2021-09-24 20:39:12 +0300

    make PostForm component responsive

--- web/src/common/PostForm.jsx
+++ web/src/common/PostForm.jsx
@@ -1,6 +1,5 @@
 import { React, useState } from 'react';
 import {
-  Grid,
   TextField,
   makeStyles,
   IconButton,
@@ -15,188 +14,172 @@ import FileUploader from './FileUploader';
 import POST_TYPES from '../constants/post-types';
 
 const useStyles = makeStyles((theme) => ({
-  modal: {
+  modalData: {
     position: 'fixed',
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
     width: `calc(90% - ${theme.spacing(20)}px)`,
-    maxHeight: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    maxHeight: '90vh',
   },
-  root: {
-    flexGrow: 1,
-  },
-  top: {
-    borderTopLeftRadius: theme.spacing(1),
-    borderTopRightRadius: theme.spacing(1),
-    backgroundColor: PRIMARY,
-    margin: theme.spacing(0),
-  },
-  middle: {
-    borderTop: 0,
-    border: `1px solid ${GRAY3}`,
-    backgroundColor: WHITE,
-    margin: theme.spacing(0),
-    maxHeight: '70vh',
-    overflowY: 'auto',
-  },
-  bottom: {
-    borderBottomLeftRadius: theme.spacing(1),
-    borderBottomRightRadius: theme.spacing(1),
-    border: `1px solid ${GRAY3}`,
-    borderTop: 0,
-    backgroundColor: WHITE,
-    margin: theme.spacing(0),
-  },
-  topbar: {
+  topContainer: {
     display: 'flex',
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
     color: WHITE,
     padding: theme.spacing(0, 2),
+    borderTopLeftRadius: theme.spacing(1),
+    borderTopRightRadius: theme.spacing(1),
+    backgroundColor: PRIMARY,
   },
-  bottomBar: {
+  middleContainer: {
+    overflowY: 'auto',
+    borderTop: 0,
+    border: `1px solid ${GRAY3}`,
+    backgroundColor: WHITE,
+    padding: theme.spacing(1, 0),
+  },
+  formDataRow: {
+    margin: theme.spacing(3, 2),
+  },
+  fileUploaderContainer: {
+    margin: theme.spacing(0, 2),
+  },
+  bottomContainer: {
     display: 'flex',
-    flexDirection: 'row',
     justifyContent: 'flex-end',
-    alignItems: 'center',
-    color: WHITE,
+    padding: theme.spacing(2),
+    borderBottomLeftRadius: theme.spacing(1),
+    borderBottomRightRadius: theme.spacing(1),
+    border: `1px solid ${GRAY3}`,
+    borderTop: 0,
+    backgroundColor: WHITE,
   },
 }));
 
-const FormContent = ({ title, btnTitle, handleClose, form, handleChangeForm, onClickBtn }) => {
+const FormDataRow = ({ children }) => (
+  <div className={useStyles().formDataRow}>
+    {children}
+  </div>
+);
+
+const FormData = ({ title, btnTitle, handleClose, form, handleChangeForm, onClickBtn }) => {
   const classes = useStyles();
   const [topicsError, setTopicsError] = useState(false);
 
   console.log(`Topics Error: ${topicsError}`);
 
   return (
-    <div>
-      <div className={classes.root}>
-        <Grid container spacing={0}>
-          <Grid container item xs={12} className={classes.top} spacing={0}>
-            <>
-              <Grid item xs={12}>
-                <div className={classes.topbar}>
-                  <h1>{title}</h1>
-                  <IconButton
-                    aria-label="close post form"
-                    style={{ color: WHITE }}
-                    onClick={handleClose}
-                  >
-                    <Close style={{ color: WHITE, width: 35, height: 35 }} />
-                  </IconButton>
-                </div>
-              </Grid>
-            </>
-          </Grid>
-          <Grid container item xs={12} className={classes.middle} spacing={3}>
-            <>
-              <Grid item xs={12}>
-                <TextField
-                  id="outlined-select-post-type"
-                  select
-                  label="Post Type"
-                  required
-                  value={form.type}
-                  onChange={(e) => handleChangeForm('type', e.target.value)}
-                  variant="outlined"
-                  fullWidth
-                  disabled={form.id !== null && form.id !== undefined}
-                >
-                  {Array.from(POST_TYPES).map(([, opt]) => (
-                    <MenuItem key={opt.value} value={opt.value}>{opt.name}</MenuItem>
-                  ))}
-                </TextField>
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  name="description"
-                  variant="outlined"
-                  required
-                  fullWidth
-                  multiline
-                  minRows={4}
-                  maxRows={4}
-                  id="description"
-                  label="Description"
-                  value={form.description}
-                  onChange={(e) => handleChangeForm('description', e.target.value)}
-                />
-              </Grid>
-              {form.type === POST_TYPES.get('bugfix').value ? (
-                <Grid item xs={12}>
-                  <TextField
-                    name="error"
-                    variant="outlined"
-                    required
-                    fullWidth
-                    multiline
-                    minRows={4}
-                    maxRows={10}
-                    id="error"
-                    label="Error"
-                    value={form.error}
-                    onChange={(e) => handleChangeForm('error', e.target.value)}
-                  />
-                </Grid>
-              ) : null}
-              {form.type === POST_TYPES.get('bugfix').value ? (
-                <Grid item xs={12}>
-                  <TextField
-                    name="solution"
-                    variant="outlined"
-                    required
-                    fullWidth
-                    multiline
-                    minRows={4}
-                    maxRows={10}
-                    id="solution"
-                    label="Solution"
-                    value={form.solution}
-                    onChange={(e) => handleChangeForm('solution', e.target.value)}
-                  />
-                </Grid>
-              ) : null}
-              <Grid item xs={12}>
-                <FileUploader
-                  files={form.images}
-                  setFiles={(images) => handleChangeForm('images', images)}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TagPicker
-                  tags={form.topics}
-                  setTags={(topics) => handleChangeForm('topics', topics)}
-                  required
-                  unique
-                  onUniqueError={(invalid) => setTopicsError(invalid)}
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TagPicker
-                  tags={form.links}
-                  setTags={(links) => handleChangeForm('links', links)}
-                  placeholder="Enter links"
-                />
-              </Grid>
-            </>
-          </Grid>
-          <Grid container item xs={12} className={classes.bottom} spacing={3}>
-            <>
-              <Grid item xs={12}>
-                <div className={classes.bottomBar}>
-                  <Button variant="contained" color="primary" onClick={onClickBtn}>
-                    {btnTitle}
-                  </Button>
-                </div>
-              </Grid>
-            </>
-          </Grid>
-        </Grid>
+    <>
+      <div className={classes.topContainer}>
+        <h1>{title}</h1>
+        <IconButton
+          aria-label="close post form"
+          style={{ color: WHITE }}
+          onClick={handleClose}
+        >
+          <Close style={{ color: WHITE, width: 35, height: 35 }} />
+        </IconButton>
       </div>
-    </div>
+      <div className={classes.middleContainer}>
+        <FormDataRow>
+          <TextField
+            id="outlined-select-post-type"
+            select
+            label="Post Type"
+            required
+            value={form.type}
+            onChange={(e) => handleChangeForm('type', e.target.value)}
+            variant="outlined"
+            fullWidth
+            disabled={form.id !== null && form.id !== undefined}
+          >
+            {Array.from(POST_TYPES).map(([, opt]) => (
+              <MenuItem key={opt.value} value={opt.value}>{opt.name}</MenuItem>
+            ))}
+          </TextField>
+        </FormDataRow>
+        <FormDataRow>
+          <TextField
+            name="description"
+            variant="outlined"
+            required
+            fullWidth
+            multiline
+            minRows={4}
+            maxRows={4}
+            id="description"
+            label="Description"
+            value={form.description}
+            onChange={(e) => handleChangeForm('description', e.target.value)}
+          />
+        </FormDataRow>
+        <FormDataRow>
+          {form.type === POST_TYPES.get('bugfix').value ? (
+            <TextField
+              name="error"
+              variant="outlined"
+              required
+              fullWidth
+              multiline
+              minRows={4}
+              maxRows={10}
+              id="error"
+              label="Error"
+              value={form.error}
+              onChange={(e) => handleChangeForm('error', e.target.value)}
+            />
+          ) : null}
+        </FormDataRow>
+        <FormDataRow>
+          {form.type === POST_TYPES.get('bugfix').value ? (
+            <TextField
+              name="solution"
+              variant="outlined"
+              required
+              fullWidth
+              multiline
+              minRows={4}
+              maxRows={10}
+              id="solution"
+              label="Solution"
+              value={form.solution}
+              onChange={(e) => handleChangeForm('solution', e.target.value)}
+            />
+          ) : null}
+        </FormDataRow>
+        <div className={classes.fileUploaderContainer}>
+          <FileUploader
+            files={form.images}
+            setFiles={(images) => handleChangeForm('images', images)}
+          />
+        </div>
+        <FormDataRow>
+          <TagPicker
+            tags={form.topics}
+            setTags={(topics) => handleChangeForm('topics', topics)}
+            required
+            unique
+            onUniqueError={(invalid) => setTopicsError(invalid)}
+          />
+        </FormDataRow>
+        <FormDataRow>
+          <TagPicker
+            tags={form.links}
+            setTags={(links) => handleChangeForm('links', links)}
+            placeholder="Enter links"
+          />
+        </FormDataRow>
+      </div>
+      <div className={classes.bottomContainer}>
+        <Button variant="contained" color="primary" onClick={onClickBtn}>
+          {btnTitle}
+        </Button>
+      </div>
+    </>
   );
 };
 
@@ -205,9 +188,9 @@ const PostForm = ({ title, btnTitle, open, setOpen, form, handleChangeForm, onCl
 
   const handleClose = () => setOpen(false);
 
-  const body = (
-    <div className={classes.modal}>
-      <FormContent
+  const ModalData = (
+    <div className={classes.modalData}>
+      <FormData
         title={title}
         btnTitle={btnTitle}
         handleClose={handleClose}
@@ -218,7 +201,7 @@ const PostForm = ({ title, btnTitle, open, setOpen, form, handleChangeForm, onCl
     </div>
   );
 
-  return <Modal open={open} onClose={handleClose} disableRestoreFocus>{body}</Modal>;
+  return <Modal open={open} onClose={handleClose} disableRestoreFocus>{ModalData}</Modal>;
 };
 
 PostForm.defaultProps = {
