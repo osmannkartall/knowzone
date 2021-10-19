556fcc3bf73d8a19439112dbf7aa4992773ef514
Osman Kartal <okartal3434@gmail.com>
2021-10-19 12:36:33 +0300

    fix the link

--- web/src/components/Dashboard/Topbar.jsx
+++ web/src/components/Dashboard/Topbar.jsx
@@ -91,7 +91,7 @@ const Topbar = ({ openSidebar }) => {
         >
           <MenuIcon />
         </IconButton>
-        <a href="http://localhost:3000" className={classes.appLogoLink}>
+        <a href={process.env.REACT_APP_KNOWZONE_FE_URI} className={classes.appLogoLink}>
           <AppLogo
             width="140"
             height="50"
