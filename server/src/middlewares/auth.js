ec8922ea34bd4a75ac6ea2df008e9cca6461593f
Osman Kartal <kartal.osman@outlook.com.tr>
2021-11-07 00:58:22 +0300

    remove unused middleware functions

--- server/src/middlewares/auth.js
+++ server/src/middlewares/auth.js
@@ -1,30 +1,3 @@
-const registerValidation = (registerApiSchema) => (req, res, next) => {
-  const validation = registerApiSchema.validate(req.body);
-  if ('error' in validation) {
-    const { error } = validation;
-    res.status(400).json({
-      status: 'fail',
-      message: error.details[0].message,
-    });
-  } else {
-    next();
-  }
-};
-
-const loginValidation = (loginApiSchema) => (req, res, next) => {
-  const validation = loginApiSchema.validate(req.body);
-
-  if ('error' in validation) {
-    const { error } = validation;
-    res.status(400).json({
-      status: 'fail',
-      message: error.details[0].message,
-    });
-  } else {
-    next();
-  }
-};
-
 const checkAuthentication = (req, res, next) => {
   if (!('userId' in req.session)) {
     res.status(401).json({
@@ -36,6 +9,4 @@ const checkAuthentication = (req, res, next) => {
   }
 };
 
-module.exports.registerValidation = registerValidation;
-module.exports.loginValidation = loginValidation;
 module.exports.checkAuthentication = checkAuthentication;
