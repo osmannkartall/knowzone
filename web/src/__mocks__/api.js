025fdd083ee5ec0451472f17b97e415deeb33c40
Osman Kartal <kartal.osman@outlook.com.tr>
2023-04-14 21:04:27 +0300

    fix mock requests

--- web/src/__mocks__/api.js
+++ web/src/__mocks__/api.js
@@ -4,37 +4,29 @@ import { forms, formTypes } from './data';
 
 const getForms = rest.get(
   `${process.env.REACT_APP_KNOWZONE_BE_URI}/${BE_ROUTES.FORMS}`,
-  async (req, res, ctx) => res(ctx.json(forms)),
+  (req, res, ctx) => res(ctx.json(forms)),
 );
 
 const createForm = rest.post(
   `${process.env.REACT_APP_KNOWZONE_BE_URI}/${BE_ROUTES.FORMS}`,
-  async (req, res, ctx) => res(ctx.json({ status: 'success', message: 'Created Successfully' })),
+  (req, res, ctx) => res(ctx.json({ status: 'success', message: 'Created Successfully' })),
 );
 
-const filterForms = rest.post(
+const getFormTypes = rest.post(
   `${process.env.REACT_APP_KNOWZONE_BE_URI}/${BE_ROUTES.FORMS}/filter`,
-  async (req, res, ctx) => {
-    const body = await req.json();
-    const isGetFormByType = body.fields && body.single === true;
-    const isGetFormTypes = JSON.stringify(body) === JSON.stringify({ projection: { type: 1 } });
+  async (req, res, ctx) => res(ctx.json(formTypes)),
+);
 
-    if (isGetFormByType) {
-      return res(ctx.json(forms[body.fields.type]));
-    }
-
-    if (isGetFormTypes) {
-      return res(ctx.json(formTypes));
-    }
-
-    return res(ctx.json(null));
-  },
+const getFormByType = rest.get(
+  `${process.env.REACT_APP_KNOWZONE_BE_URI}/${BE_ROUTES.FORMS}`,
+  (req, res, ctx) => res(ctx.json(forms[req.url.searchParams.get('type')])),
 );
 
 const api = [
   getForms,
   createForm,
-  filterForms,
+  getFormTypes,
+  getFormByType,
 ];
 
 export default api;
