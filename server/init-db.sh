39316b7bd1ab54091e201f5ff8fc7038119f963e
Samet Öztürk <sametozturk9@gmail.com>
2023-04-20 04:49:43 +0300

    enable transactions in mongo db

---
+++ server/init-db.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+docker run -d \
+  -p 27017:27017 \
+  --name dev-mongo \
+  -v dev-mongo-data:/data/db \
+  mongo:latest \
+  mongod --replSet rs0
+
+docker exec dev-mongo \
+  mongosh --eval "rs.initiate({ _id: 'rs0', members: [{ _id: 0, host: 'localhost:27017' }] })"
